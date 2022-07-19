fe791de79a5b2d731857c2008d071da49df0d100
hirunppal <hirunppal@gmail.com>
2022-07-19 16:48:27 +0700

    feat: uiHtmlQuiz1

---
+++ Quiz1/exercise_6.php
@@ -0,0 +1,39 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+
+<body>
+<form >
+  <label for="name" >ชื่อ นามสกุล  </label> <input></input>
+<div>
+
+</div>
+<div class="button"> </div>
+</form>
+
+
+<?php 
+
+?>
+</body>
+</html>
