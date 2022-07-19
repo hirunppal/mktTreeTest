60970a59e197e90a1ffdcfe73d6f2c0fad22ed6a
hirunppal <hirunppal@gmail.com>
2022-07-19 23:57:09 +0700

    featlquiz1

--- Quiz1/exercise_6.php
+++ Quiz1/exercise_6.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "Sql1150*";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -11,7 +11,20 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$firstname =$_POST["name"]
+$email =$_POST["email"]
+$phoneNum =$_POST["phoneNum"]
+$salary =$_POST["salary"]
+$education =$_POST["education"]
+$gender =$_POST["gender"]
+$age =$_POST["age"]
+
+if(isset($_POST['buttonsubmit'])) {
+  echo "This is Button2 that is selected";
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,17 +32,42 @@ echo "Connected successfully";
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="ex6styles.css">
 </head>
 
 
 <body>
-<form >
-  <label for="name" >ชื่อ นามสกุล  </label> <input></input>
-<div>
+  <form class="form-controll" action="exercise_6.php" medthid="POST" >
+    <div>
+    <label for="name" >ชื่อ นามสกุล  </label> <input id="name"></input>
+    <label for="email" type="email">อีเมล  </label> <input id="email"></input>
+    <label for="phoneNum" type="number" >เบอร์โทร  </label> <input id="phoneNum"></input>
+    <label for="salary" type="number">เงินเดือน  </label>
+    <select name="salary" id="salary">
+      <option value="0-15000">0 - 15,000 บาท</option>
+      <option value="15001-30000">15,001 - 30,000 บาท</option>
+      <option value="30000">30,000 บาท ขึ้นไป</option>
+    </select>
+    <label for="education" >การศึกษา  </label>
+    <select name="education" id="education">
+      <option value="bachelor"> ป. ตรี</option>
+      <option value="master"> ป. โท</option>
+      <option value="doctor">  ป. เอก</option>
+    </select>
+    <label for="gender" >เพศ  </label> 
+    <div class="genderinput">
+      <input type="radio" name="gender" value="male">ชาย</input> <input type="radio" name="gender" value="c">หญิง</input>
+    </div>
+    <label for="age" >อายุ  </label> <input id="age"  type="number"></input>
+    
+    <div class="butSubmit">
+       <button        type=" submit"  name="buttonsubmit"
+                value="buttonsubmit" >บันทึก</button>
+      </div>
 
-</div>
-<div class="button"> </div>
-</form>
+  </div>
+  </form>
+  
 
 
 <?php 
